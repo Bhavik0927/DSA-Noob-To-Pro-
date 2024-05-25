47ae398b34f00180b371ddc2cc4c2b755abb94cb
Bhavik <bhaviktembhare2@gmail.com>
2024-05-25 11:09:06 +0530

    Easy Level Arrays Questions

---
+++ Arrays/Easy Level/Union.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Union {
+
+    // brute force method
+    static ArrayList<Integer> UnionArray(int[] arr1, int[] arr2, int n, int m) {
+
+        HashSet<Integer> s = new HashSet<>();
+        ArrayList<Integer> union = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            s.add(arr1[i]);
+        }
+
+        for (int j = 0; j < m; j++) {
+            s.add(arr2[j]);
+        }
+
+        for (int it : s) {
+            union.add(it);
+        }
+
+        return union;
+    } 
+
+    // Optimal Approch
+    static ArrayList<Integer> UnionOfArray(int[] arr1, int[] arr2, int n, int m) {
+        ArrayList<Integer> union = new ArrayList<>();
+
+        int i = 0;
+        int j = 0;
+        while (i < m && j < n) {
+            if (arr1[i] < arr2[j]) {
+                if (union.size() == 0 || union.get(union.size() - 1) != arr1[i]) {
+                    union.add(arr1[i]);
+                }
+                i++;
+            } else {
+                if (union.size() == 0 || union.get(union.size() - 1) != arr2[j]) {
+                    union.add(arr2[j]);
+                }
+                j++;
+            }
+        }
+        return union;
+    }
+
+    public static void main(String[] args) {
+        int n = 10, m = 7;
+        int arr1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int arr2[] = { 2, 3, 4, 4, 5, 11, 12, 13, 11 };
+
+        ArrayList<Integer> union = UnionOfArray(arr1, arr2, n, m);
+
+        for (int un : union) {
+            System.out.println(un);
+        }
+    }
+}
---
+++ Arrays/Easy Level/moveZero.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class moveZero {
+
+    // brute force method
+    static int[] moveZeres(int[] arr, int n) {
+
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                temp.add(arr[i]);
+            }
+        }
+
+        int nz = temp.size(); // get the size of the temp;
+
+        for (int i = 0; i < nz; i++) { // add nz value in array
+            arr[i] = temp.get(i);
+        }
+
+        for (int i = nz; i < n; i++) {
+            arr[i] = 0;
+        }
+
+        return arr;
+    }
+
+    // Optimal method
+
+    static int[] moveZeres(int[] arr, int n) {
+
+        // first we find 0 in array
+        int j = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        for (int i = j + 1; i < n; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 0, 2, 3, 2, 0, 0, 4, 5, 1 };
+        int n = 10;
+        int[] ans = moveZeres(arr, n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println("");
+    }
+}
