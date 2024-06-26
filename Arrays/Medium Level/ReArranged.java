40dec8fd95cdfcf0de3b820cf473e3b65eadf19f
Bhavik <bhaviktembhare2@gmail.com>
2024-06-26 14:44:18 +0530

    Optimal Approch is added

--- Arrays/Medium Level/ReArranged.java
+++ Arrays/Medium Level/ReArranged.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
 public class ReArranged {
-    static int[] reArrangedBySign(int[] arr, int n) {
+
+    // Brute Force method
+    /* static int[] reArrangedBySign(int[] arr, int n) {
 
         ArrayList<Integer> pos = new ArrayList<>();
         ArrayList<Integer> neg = new ArrayList<>();
@@ -19,13 +21,37 @@ public class ReArranged {
             arr[2 * i + 1] = neg.get(i);
         }
         return arr;
+    } */
+
+    // Optimal Approch
+    
+    public static int[] RearrangedBySign(int[] A){
+        int n = A.length;
+
+        int[] arr = new int[n];
+
+        int posIndex = 0 , negIndex = 1;
+        
+        for(int i = 0; i< n; i++){
+            if(A[i] < 0){
+                arr[negIndex]  = A[i];
+                negIndex += 2;
+            }else{
+                arr[posIndex] = A[i];
+                posIndex += 2;
+            }
+        }
+
+        return arr;
     }
 
     public static void main(String[] args) {
         int n = 4;
         int A[] = { 1, 2, -4, -5 };
 
-        int[] ans = reArrangedBySign(A, n);
+        // int[] ans = reArrangedBySign(A, n); // for brute force
+
+        int[] ans = RearrangedBySign(A);
 
         for(int i = 0; i< ans.length; i++){
             System.out.print(ans[i] + " ");
