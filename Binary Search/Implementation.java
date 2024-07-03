073bf874fc1936e50a30216e71b42791804b0fa5
Bhavik <bhaviktembhare2@gmail.com>
2024-07-03 14:31:14 +0530

    Add Both the Approches

--- Binary Search/Implementation.java
+++ Binary Search/Implementation.java
@@ -1,4 +1,6 @@
 public class Implementation {
+
+    // Iterative Approch
     static int binarySearch(int[] arr, int target) {
         int n = arr.length;
         int low = 0, high = n - 1;
@@ -15,8 +17,22 @@ public class Implementation {
             }
         }
         return -1;
+    } 
+
+    // Recursive Approch
+    static int Search(int[] arr,int low,int high, int target){
+        if(low > high) return -1;
+
+        int mid = (low + high) / 2;
+
+        if(arr[mid] == target) return mid;
+        else if (arr[mid] > target) return Search(arr, low, mid-1, target);
+        else return Search(arr, mid + 1, high, target);
     }
 
+    static int binarySearch(int[] arr,int target){
+        return Search(arr, 0, arr.length - 1, target);
+    }
     public static void main(String[] args) {
         int[] a = { 3, 4, 6, 7, 9, 12, 16, 17 };
         int target = 6;
