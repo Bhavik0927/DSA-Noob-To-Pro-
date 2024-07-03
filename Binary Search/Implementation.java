7e0cd761d689d8ce8e15acda12e7b92b1dad82f1
Bhavik <bhaviktembhare2@gmail.com>
2024-07-03 14:17:56 +0530

    Implementation of Binary Search

---
+++ Binary Search/Implementation.java
@@ -0,0 +1,29 @@
+public class Implementation {
+    static int binarySearch(int[] arr, int target) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 3, 4, 6, 7, 9, 12, 16, 17 };
+        int target = 6;
+        int ind = binarySearch(a, target);
+        if (ind == -1)
+            System.out.println("The target is not present.");
+        else
+            System.out.println("The target is at index: " + ind);
+    }
+}
