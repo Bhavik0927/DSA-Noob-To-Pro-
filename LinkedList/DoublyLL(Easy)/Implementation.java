70d4c82dda2bffc14431e1b8c2e16b2ea0c5e985
Bhavik <bhaviktembhare2@gmail.com>
2024-07-23 11:08:36 +0530

    insert method included

--- LinkedList/DoublyLL(Easy)/Implementation.java
+++ LinkedList/DoublyLL(Easy)/Implementation.java
@@ -14,11 +14,26 @@ public class Implementation {
     Node head;
     Node tail;
 
+    void Add(int elm){
+        Node temp = new Node(elm);
+        if(head == null){
+            head = temp;
+            tail = temp;
+        }else{
+            tail.prev = temp;
+            temp.next = tail;
+            tail = temp;
+        }
+    }
+
     public static void main(String[] args) {
-        Node temp = new Node(10);
-        
-        System.out.println(temp.data);
-        System.out.println(temp.prev);
-        System.out.println(temp.next);
+        Implementation dll = new Implementation();
+        dll.Add(20);
+        dll.Add(34);
+
+        System.out.println(dll.head.data);
+        System.out.println(dll.tail.data);
+
+
     }
 }
