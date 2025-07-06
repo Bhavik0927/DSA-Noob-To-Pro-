2fa2b43cc19fa9571a1b398597fdb990306b1c00
Bhavik <bhaviktembhare2@gmail.com>
2025-07-06 18:53:01 +0530

    Rat in maze path

--- Recursion/Backtracting.js
+++ Recursion/Backtracting.js
@@ -1,5 +1,6 @@
 // Phone Keypad Combination
 
+/* 
 const Get_Combination = (digits, index, map, result, current) => {
 
     if (index === digits.length) {
@@ -38,3 +39,55 @@ const PhoneCombination = (digits) => {
 }
 
 console.log(PhoneCombination('678'));
+
+*/
+
+
+// Rat in Maze path
+
+
+const Solve_Maze = (maze, x, y, visited, path, result, dir, dx, dy) => {
+    const n = maze.length;
+
+    if (x === n - 1 && y === n - 1) {
+        result.push(path);
+        return;
+    }
+
+    for (let i = 0; i < 4; i++) {
+        const newX = x + dx[i];
+        const newY = y + dy[i];
+        const move = dir[i];
+
+        if ( newX >= 0 && newY >= 0 &&
+             newX < n && newY < n &&
+             maze[newX][newY] === 1 &&
+             !visited[newX][newY] ) 
+        {
+            visited[x][y] = true;
+            Solve_Maze(maze, newX, newY, visited, path + move,result, dir, dx, dy);
+            visited[x][y] = false;
+        }
+    }
+}
+
+const Rat_In_Maze = (maze) => {
+    const n = maze.length;
+    let result = [];
+
+    if (maze[0][0] === 0) return [];
+
+    const visited = Array.from({ length: n }, () => Array(n).fill(false));
+
+    const dir = ['D', 'L', 'R', 'U'];
+    const dx = [1, 0, 0, -1];
+    const dy = [0, -1, 1, 0];
+
+    Solve_Maze(maze, 0, 0, visited, '', result, dir, dx, dy);
+
+    return result;
+}
+
+const Maze = [ [1,0,0,0],[1,1,0,1],[1,1,0,0],[0,1,1,1] ];
+
+console.log(Rat_In_Maze(Maze));
