850294a4e350dc066e41b886eca2443af14af587
Bhavik <bhaviktembhare2@gmail.com>
2025-08-19 08:05:36 +0530

    checked Balance tree

---
+++ Tree/CheckBalance.js
@@ -0,0 +1,28 @@
+class Node{
+    constructor(root){
+        this.val = root;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+const height = (node) =>{
+    if(node === null) return 0;
+
+    return 1 + Math.max( (height.left),(height.right)  );
+}
+
+const balacedCheck = (root) =>{
+    if(root === null) return false;
+
+    let leftNode = height(root.left);
+    let rightNode = height(root.right);
+
+    if(Math.abs(leftNode - rightNode) > 1){
+        return false;
+    }
+
+
+    return balacedCheck(root.left) && balacedCheck(root.right);
+}
