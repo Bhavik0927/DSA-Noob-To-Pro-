39290ee07f6a0f25b97da36e1c5eb20bdd516d12
Bhavik Tembhare <bhaviktembhare2@gmail.com>
2025-11-03 15:01:29 +0530

    Refactor Min Stack implementation to improve push, pop, peek, and get_Min functions

--- StackQ/Min_Stack.js
+++ StackQ/Min_Stack.js
@@ -1,10 +1,26 @@
-const Create_MinStack = () =>{
-    let stack = [];
-    let minStack = [];
+// This Problem is solved in O(1) time complexity
+// Space Complexity = O(n);
 
-    return {
-        push:{
-            
-        }
+const stack = [];
+
+const push = (val) =>{
+    if(stack.lenght === 0){
+        stack.push[val, val];
+    }else{
+
+        let minVal = Math.min(val, stack[stack.length - 1][1]);
+        stack.push(val, minVal);
     }
+};
+
+const pop = () =>{
+    return stack.pop();
+}
+
+const peek = () =>{
+    return stack[stack.length - 1][0];
+}
+
+const get_Min = () =>{
+    return stack[stack.length - 1][1];
 }
