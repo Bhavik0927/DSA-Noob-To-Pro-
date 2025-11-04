40e90f927a3b42aaeb10e9bba93b8d3c6db166f9
Bhavik Tembhare <bhaviktembhare2@gmail.com>
2025-11-04 15:26:18 +0530

    Add function to remove outermost parentheses from a string

---
+++ StackQ/RemoveOutermost.js
@@ -0,0 +1,26 @@
+
+
+const RemoveOuterMost_Paranthesis = (s) =>{
+    let stack = [];
+    let ans = "";
+
+    for(let i = 0; i< s.length; i++){
+        if(s[i] === '('){
+            stack.push(s[i]);
+            if(stack.length > 1){
+                ans += s[i];
+            }
+        }else{
+            if(stack.length > 1) ans += s[i];
+
+            stack.pop();
+        }
+    }
+
+    return ans;
+}
+
+
+const s = "(()())(())(()(()))";
+
+console.log(RemoveOuterMost_Paranthesis(s));
